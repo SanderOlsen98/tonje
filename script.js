b70a24236a8af65d9c85db0ea47d7b0dc2a84359
SanderOlsen98 <sanderlolsen@gmail.com>
2022-07-15 22:38:48 +0200

    up

--- script.js
+++ script.js
@@ -3,27 +3,27 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "22 Dec 2021";
+const newYears = "22 Dec 2022";
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
-    const currentDate = new Date();
+  const newYearsDate = new Date(newYears);
+  const currentDate = new Date();
 
-    const totalSeconds = (newYearsDate - currentDate) / 1000;
+  const totalSeconds = (newYearsDate - currentDate) / 1000;
 
-    const days = Math.floor(totalSeconds / 3600 / 24);
-    const hours = Math.floor(totalSeconds / 3600) % 24;
-    const mins = Math.floor(totalSeconds / 60) % 60;
-    const seconds = Math.floor(totalSeconds) % 60;
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const mins = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
 
-    daysEl.innerHTML = days;
-    hoursEl.innerHTML = formatTime(hours);
-    minsEl.innerHTML = formatTime(mins);
-    secondsEl.innerHTML = formatTime(seconds);
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = formatTime(hours);
+  minsEl.innerHTML = formatTime(mins);
+  secondsEl.innerHTML = formatTime(seconds);
 }
 
 function formatTime(time) {
-    return time < 10 ? `0${time}` : time;
+  return time < 10 ? `0${time}` : time;
 }
 
 // initial call
@@ -46,75 +46,75 @@ let x = undefined;
 let y = undefined;
 
 canvas.addEventListener("mousedown", (e) => {
-    isPressed = true;
+  isPressed = true;
 
-    x = e.offsetX;
-    y = e.offsetY;
+  x = e.offsetX;
+  y = e.offsetY;
 });
 
 canvas.addEventListener("mouseup", (e) => {
-    isPressed = false;
+  isPressed = false;
 
-    x = undefined;
-    y = undefined;
+  x = undefined;
+  y = undefined;
 });
 
 canvas.addEventListener("mousemove", (e) => {
-    if (isPressed) {
-        const x2 = e.offsetX;
-        const y2 = e.offsetY;
+  if (isPressed) {
+    const x2 = e.offsetX;
+    const y2 = e.offsetY;
 
-        drawCircle(x2, y2);
-        drawLine(x, y, x2, y2);
-        x = x2;
-        y = y2;
-    }
+    drawCircle(x2, y2);
+    drawLine(x, y, x2, y2);
+    x = x2;
+    y = y2;
+  }
 });
 
 function drawCircle(x, y) {
-    ctx.beginPath();
-    ctx.arc(x, y, size, 0, Math.PI * 2);
-    ctx.fillStyle = color;
-    ctx.fill();
+  ctx.beginPath();
+  ctx.arc(x, y, size, 0, Math.PI * 2);
+  ctx.fillStyle = color;
+  ctx.fill();
 }
 
 function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.strokeStyle = color;
-    ctx.lineWidth = size * 2;
-    ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.strokeStyle = color;
+  ctx.lineWidth = size * 2;
+  ctx.stroke();
 }
 
 increaseBtn.addEventListener("click", () => {
-    size += 5;
+  size += 5;
 
-    if (size > 50) {
-        size = 50;
-    }
+  if (size > 50) {
+    size = 50;
+  }
 
-    updateSizeOnScreen();
+  updateSizeOnScreen();
 });
 
 decreaseBtn.addEventListener("click", () => {
-    size -= 5;
+  size -= 5;
 
-    if (size < 5) {
-        size = 5;
-    }
+  if (size < 5) {
+    size = 5;
+  }
 
-    updateSizeOnScreen();
+  updateSizeOnScreen();
 });
 
 colorEl.addEventListener("change", (e) => {
-    color = e.target.value;
+  color = e.target.value;
 });
 
 clearEl.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
 function updateSizeOnScreen() {
-    sizeEl.innerText = size;
+  sizeEl.innerText = size;
 }
