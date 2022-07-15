711788bff2ae33578e4c3085455f2453acb58a98
SanderOlsen98 <sanderlolsen@gmail.com>
2022-07-15 22:37:20 +0200

    update

---
+++ aalesund.js
@@ -0,0 +1,27 @@
+const daysEll = document.getElementById("days1");
+const hoursEll = document.getElementById("hours1");
+const minsEll = document.getElementById("mins1");
+const secondsEll = document.getElementById("seconds1");
+
+const newYears = "27 Dec 2021";
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEll.innerHTML = days;
+    hoursEll.innerHTML = formatTime(hours);
+    minsEll.innerHTML = formatTime(mins);
+    secondsEll.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
